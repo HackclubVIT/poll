052c2a2e0d5d020c84f135e5e9ecd5b46bd7cdc3
Subhendu Dash <sdash29102@gmail.com>
2023-03-31 01:28:13 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ async function remove_votes(reg_no) {
     quote_id: reg_no,
     increment_num: 1,
   });
-  alert("Unvoted" + reg_no);
+  alert("Unvoted " + reg_no);
 }
 
 async function update_votes(reg_no) {
@@ -65,11 +65,7 @@ async function update_votes(reg_no) {
       quote_id: reg_no,
       increment_num: 1,
     });
-    const { data2, error2 } = await _supabase.rpc("vote_config_insert", {
-      email: session.user.email,
-      participant_regno: reg_no,
-    });
-    alert("You voted" + reg_no);
+    alert("You voted " + reg_no);
   }
   else {
     alert("Sign in to vote first!");
